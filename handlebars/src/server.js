743305a87cbd4bd981a950dd80e7b6452a7f1765
laureanorugani <laureanorugani@gmail.com>
2022-10-31 16:05:06 -0300

    desafio 06

--- handlebars/src/server.js
+++ handlebars/src/server.js
@@ -1,42 +1,51 @@
-const express = require('express');
-const {engine} = require('express-handlebars');
-const Contenedor = require('./contenedor')
-const contenedor = new Contenedor("productos.json");
-const app = express();
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>MyAPI - Demarchi</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link href="./styles.css" rel="stylesheet" />
+        <script src ="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="/socket.io/socket.io.js"></script>
+    </head>
+    <body>
+        <div class="d-flex" id="wrapper">
+            <div class="border-end bg-white" id="sidebar-wrapper">
+                <div class="sidebar-heading border-bottom bg-light">API Productos</div>
+                <div class="list-group list-group-flush">
+                </div>
+            </div>
+            <div id="page-content-wrapper">
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
-
-app.set('views', './src/views');
-app.set('view engine', 'hbs');
-
-app.engine('hbs', engine({
-    extname: '.hbs',
-    defaultLayout: 'index.hbs',
-    layoutsDir: __dirname + '/views/layouts',
-    partialsDir: __dirname + '/views/partials'
-}))
-
-app.get('/productos', async(req, res) => {
-    const productos = await contenedor.getAll();
-    res.render('pages/list', {productos})
-})
-
-app.post('/productos', async(req,res) => {
-    const {body} = req;
-    await contenedor.save(body);
-    res.redirect('/');
-})
-
-app.get('/', (req,res) => {
-    res.render('pages/form', {})
-})
-
-
-const PORT = 1242;
-const server = app.listen(PORT, () => {
-console.log(` >>>>> 🚀 Server started at http://localhost:${PORT}`)
-})
-
-server.on('error', (err) => console.log(err))
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <div class="container-fluid">
+                        <button class="btn btn-primary btn-sm" id="sidebarToggle">Toggle Menu</button>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                                <li class="nav-item active"><a class="nav-link" href="#!">Home</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#!">Link</a></li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#!">Action</a>
+                                        <a class="dropdown-item" href="#!">Another action</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#!">Something else here</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div class="container-fluid px-5 pt-3">
+                </div>
+            </div>
+        </div>
+        <script src="./js/bootstrap.bundle.min.js"></script>
+        <script src="./js/scripts.js"></script>
+        
+    </body>
+</html>
