f8ece59101a224d829d78c37d146149ea4624759
laureanorugani <laureanorugani@gmail.com>
2023-01-17 14:13:41 -0300

    desafio 16

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { CarritoDao } from "../dao/CarritoDao.js";
 import { ProductoDao } from "../dao/ProductoDao.js";
+import logger from "../loggers/Log4jsLogger.js";
 
 const router = express.Router();
 const carritoDao = new CarritoDao();
 
-// POST /api/carrito
 router.post('/', async (_req, res) => {
     const newCart = await carritoDao.createCart();
     
@@ -15,7 +15,6 @@ router.post('/', async (_req, res) => {
     
 })
 
-// DELETE /api/carrito/id
 router.delete('/:id', async(req,res) => {
     const { id } = req.params;
     const wasDeleted = await carritoDao.deleteCartById(id);
@@ -23,10 +22,9 @@ router.delete('/:id', async(req,res) => {
     wasDeleted 
         ? res.status(200).json({"success": "cart successfully removed"})
         : res.status(404).json({"error": "cart not found"})
-     
+
 })
 
-// POST /api/carrito/:id/productos
 
 router.post('/:id/productos', async(req,res) => {
     const { id } = req.params;
